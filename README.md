5cc35667966f5761171ebf6ac48f2a1148df3578
Sandra Amurao <sandramae421@gmail.com>
2026-04-15 21:34:35 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+<<<<<<< HEAD
 # revel
 An AI-powered tool that analyzes API request/response data, detects inconsistencies, and explains issues with suggested fixes. Built to streamline debugging using JSON diffing and AI insights.
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+function NavBar() {
+
+  return (
+    <>
+    <div className="flex flex-col sm:flex-row justify-between border border-[#2e2e2e] bg-[#091824] shadow-lg shadow-[#2a5ab3]/10 p-4"> 
+      <div> Revel </div>
+    </div>
+    </>
+  )
+}
+
+export default NavBar
