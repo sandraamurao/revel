0dd04f4398016ebeeeb50ce2c56bfe08c0419f12
Sandra Amurao <sandramae421@gmail.com>
2026-05-01 09:53:06 +0100

    refactored code for analysis panel

--- src/components/AnalysisPanel.tsx
+++ src/components/AnalysisPanel.tsx
@@ -1,17 +1,27 @@
 import { useApiState } from "../store/useStore";
+import { AlertTriangle, XCircle } from "lucide-react";
+import type { Issue } from "../utils/compareJson";
 
 function AnalysisPanel() {
 	const issues = useApiState((state) => state.issues);
 	const error = useApiState((state) => state.error);
 	const sourceOfTruth = useApiState((state) => state.sourceOfTruth);
 
-	const issueConfig = {
-		"Naming mismatch": { icon: "⚠️", color: "yellow" },
-		"Missing field": { icon: "❌", color: "#a71010" },
-		"Type mismatch": { icon: "⚠️", color: "orange" },
+	const colorCodes = {
+		"Naming mismatch": "border-[#ffee00]",
+		"Missing field": "border-[#aa1e1e]",
+		"Type mismatch": "border-[#ff9100]",
 	};
 
-	console.log("issues ", issues);
+	function getDisplayMessage(issue: Issue) {
+		if (issue.issue === "Naming mismatch")
+			return `${sourceOfTruth} expects ${issue.expected}, but ${sourceOfTruth === "Response" ? "request" : "response"} uses ${issue.actual}`;
+		if (issue.issue === "Missing field")
+			return `Field exists in ${sourceOfTruth.toLowerCase()} but missing in ${sourceOfTruth === "Response" ? "request" : "response"}`;
+		if (issue.issue === "Type mismatch")
+			return `Expected ${issue.expected.toLowerCase()}, received ${issue.actual.toLowerCase()} in ${sourceOfTruth === "Response" ? "request" : "response"}`;
+	}
+
 	return (
 		<>
 			<div className="border border-[#474747] bg-[#0b426d]/15 rounded-[27px] p-6 mb-5">
@@ -20,6 +30,7 @@ function AnalysisPanel() {
 						<div> {error} </div>
 					) /* Displays "Invalid JSON" message */
 				}
+
 				{issues.length > 0 && (
 					<>
 						<h1> Analysis </h1>
@@ -28,77 +39,32 @@ function AnalysisPanel() {
 						<div>
 							{issues.map((issue, index) => (
 								<div key={index} className="mb-3">
-									{issue.issue === "Naming mismatch" && (
-										<>
-											{" "}
-											<div
-												className={`flex flex-col border rounded-lg p-3 bg-[#1a1f31]`}
-												style={{
-													borderColor: issueConfig["Naming mismatch"].color,
-												}}
-											>
-												<div className="flex flex-row  items-center gap-3 ">
-													<div> {issueConfig["Naming mismatch"].icon} </div>
-													<div className="text-[#5280ff] font-mono"> {issue.field} </div>
-													<div className="bg-[#7c7676] p-[6px] text-[10px] font-bold rounded-lg">
-														{" "}
-														{issue.issue.toLowerCase()}{" "}
-													</div>
-												</div>
-												<div>
-													{sourceOfTruth} expects {issue.expected}, but
-													{sourceOfTruth === "Response" && <> request </>}
-													{sourceOfTruth === "Request" && <> response </>} uses {issue.actual}
-												</div>
-											</div>
-										</>
-									)}
-									{issue.issue === "Missing field" && (
-										<>
-											<div
-												className={`flex flex-col gap-2 border rounded-lg p-2 bg-[#1a1f31]`}
-												style={{
-													borderColor: issueConfig["Missing field"].color,
-												}}
-											>
-												<div className="flex flex-row  items-center gap-3">
-													<div> {issueConfig["Missing field"].icon} </div>
-													<div className="text-[#5280ff] font-mono"> {issue.field} </div>
-													<div className="bg-[#7c7676] p-[6px] text-[10px] font-bold rounded-lg">
-														{issue.issue.toLowerCase()}
-													</div>
-												</div>
+									<div
+										className={`flex flex-col border ${colorCodes[issue.issue as keyof typeof colorCodes]} rounded-lg p-3 bg-[#1a1f31]`}
+									>
+										<div className="flex flex-row items-center gap-3">
+											{/* Icons */}
+											{issue.issue == "Naming mismatch" ||
+											issue.issue == "Type mismatch" ? (
+												<AlertTriangle className="w-4 h-4 text-yellow-500" />
+											) : (
+												<XCircle className="w-4 h-4 text-[#db2020]" />
+											)}
 
-												<div>
-													Field exists in {sourceOfTruth.toLowerCase()} but missing in
-													{sourceOfTruth === "Response" && <> request </>}
-													{sourceOfTruth === "Request" && <> response </>}
-												</div>
+											{/* Issue.field */}
+											<div className="text-[#5280ff] font-mono">
+												{issue.field}
 											</div>
-										</>
-									)}
-									{issue.issue === "Type mismatch" && (
-										<div
-											className={`flex flex-col gap-2 border rounded-lg p-2 bg-[#1a1f31]`}
-											style={{
-												borderColor: issueConfig["Type mismatch"].color,
-											}}
-										>
-											<div className="flex flex-row items-center gap-3">
-												<div> {issueConfig["Type mismatch"].icon} </div>
-												<div className="text-[#5280ff] font-mono"> {issue.field} </div>
-												<div className="bg-[#7c7676] p-[6px] text-[10px] font-bold rounded-lg">
-													{issue.issue.toLowerCase()}
-												</div>
-											</div>
-											<div>
-												Expected {issue.expected.toLowerCase()}, received{" "}
-												{issue.actual.toLowerCase()} in
-												{sourceOfTruth === "Response" && <> request </>}
-												{sourceOfTruth === "Request" && <> response </>}
+
+											{/* Issue meassage */}
+											<div className="bg-[#7c7676] p-[6px] ml-2 text-[10px] font-bold rounded-lg">
+												{issue.issue.toLowerCase()}
 											</div>
 										</div>
-									)}
+
+										{/* display message based on the issue */}
+										<div> {getDisplayMessage(issue)} </div>
+									</div>
 								</div>
 							))}
 						</div>
