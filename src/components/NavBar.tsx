0cec0adaf2d8852f75963ce73b2f9f573b941b87
Sandra Amurao <sandramae421@gmail.com>
2026-05-27 20:54:14 +0100

    added logo image

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,10 @@ function NavBar() {
   return (
     <>
     <div className="flex flex-col sm:flex-row justify-between border-b border-[#474747] bg-[#0f2a3f]/50 shadow-lg shadow-[#2a5ab3]/10 p-4"> 
-      <div> Revel </div>
+      <div className="flex flex-row items-center gap-3">
+        <img src="src\assets\revel-logo.png" alt="" width={40} />
+         <h1 className="font-bold">Revel</h1> 
+      </div>
     </div>
     </>
   )
