e89a7f5e48013ce83babb62e25549c8f245b71e5
Sandra Amurao <sandramae421@gmail.com>
2026-06-03 20:51:41 +0100

    use correct asset paths for logo in navbar and favicon

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,10 @@
-import revelLogo from "../assets/revel-logo.png"
-
 function NavBar() {
 
   return (
     <>
     <div className="flex flex-col sm:flex-row justify-between border-b border-[#474747] bg-[#0f2a3f]/50 shadow-lg shadow-[#2a5ab3]/10 p-4"> 
       <div className="flex flex-row items-center gap-3">
-        <img src={revelLogo} alt="Revel Logo" width={40} />
+        <img src="/revel-logo.png" alt="Revel Logo" width={40} />
          <h1 className="font-bold">Revel</h1> 
       </div>
     </div>
